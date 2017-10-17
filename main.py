3a5f3ff582a269aa1f1e0042a1a3a8da94883075
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2017-10-16 19:44:22 -0500

    Working correctly

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:95vtec@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
+app.secret_key = 'asdfasfasfd'
 #create db object
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
 
-    id= db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(230))
 
@@ -22,20 +23,33 @@ class Blog(db.Model):
 def index():
     return render_template('newpost.html')
 
-@app.route('/blog', methods=['POST'])
+@app.route('/blog', methods=['POST', 'GET'])
 def entry():
+    if request.method == 'POST':
+        title = request.form['blog_title']  #pull title from form
+        body = request.form['blog_entry']   #pull body from from
 
-    #blog_id = int(request.form['blog-id'])
-    title = request.form['blog_title']
-    body = request.form['blog_entry']
+        if title == '':
+            flash("Please fill out all fields!", 'error')
+            return redirect ('/newpost')
+        if body == '':
+            flash("Please fill out all fields!", 'error')
+            return redirect ('/newpost')
 
+        new_blog = Blog(title,body)  #create new blog object
+        db.session.add(new_blog)
+        db.session.flush()
+        blogsId = new_blog.id  #assigns id to new blog
+        db.session.commit()  #commits blog to db
+        return redirect ('/blog?id={0}'.format(blogsId))
+
+    x = request.args.get('id')
+    # checks if there are query parameters
+    if x:
+        blogId = Blog.query.get(request.args.get('id'))
+        return render_template('soloblog.html', blogId=blogId)
     blogs = Blog.query.all()
-    #blogs.body = body
-    new_blog = Blog(title,body)
-    db.session.add(new_blog)
-    db.session.commit()
-
-    return render_template('blog.html', titles="Build-a-blog", title=title, body=body, blogs=blogs)
+    return render_template('blog.html', titles="Build-a-blog", blogs=blogs)
 
 if __name__== "__main__":
     app.run()
